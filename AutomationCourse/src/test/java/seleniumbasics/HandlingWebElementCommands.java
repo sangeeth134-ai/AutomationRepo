5f977da85ee15f49e393d131454943312d1a6329
silpasureshbabu <silpasureshbabu@localhost>
2026-03-25 17:58:07 -0700

    latest push for handling webelements

--- AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
@@ -1,7 +1,15 @@
 package seleniumbasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElementCommands  extends Base {
 
@@ -16,6 +24,16 @@ public class HandlingWebElementCommands  extends Base {
 	System.out.println(button1.isDisplayed());
 	System.out.println(button1.isEnabled());
 
+	//apply explicit wait
+	WebDriverWait wait = new  WebDriverWait(driver, Duration.ofSeconds(5));
+	wait.until(ExpectedConditions.elementToBeClickable(button1));
+	
+	//fluent Wait
+	
+	Wait<WebDriver> fluent = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5)).pollingEvery(Duration.ofSeconds(1))
+			.ignoring(NoSuchElementException.class);
+	fluent.until(ExpectedConditions.elementToBeClickable(button1));
+	
 	button1.click();
 	
 	WebElement yourmessage = driver.findElement(By.id("message-one"));
